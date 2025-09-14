cba440077b6808b2ef03e770f39ee0eb5928e46d
Manoj Pipare <manojvipipare@gmail.com>
2025-09-14 23:44:48 +0530

    Initial commit

---
+++ frontend/BusinessPrediction/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-blue-400'>hello worlds</h1>
+    </div>
+  )
+}
+
+export default App
